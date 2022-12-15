a3478d72a4f4ef80ea8fcb8e1f7ab2e27192a5ea
Max Sidorenko <fivemoshr@gmail.com>
2022-12-15 17:20:11 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,10 @@ export class App extends Component {
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: 'id-5', name: 'Larry Copeland', number: '287-91-26' },
+      { id: 'id-6', name: 'Eden Copeland', number: '227-91-26' },
+      { id: 'id-7', name: 'Annie Eden', number: '247-91-26' },
+      { id: 'id-8', name: 'Clement Copeland', number: '297-91-26' },
     ],
     filter: '',
   };
